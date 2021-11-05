007436d586876897c0e866fdd7d66fea95557405
shashank-16 <shashank.chaurasia1606@gmail.com>
2021-11-05 21:32:25 +0530

    deleting things

--- process_file/candidate-signup.php
+++ process_file/candidate-signup.php
@@ -9,15 +9,19 @@
         $branch=$_POST["branch"];
         $fname=$_POST["F_name"];
         $lname=$_POST["L_name"];
+
+        $full_name=$fname." ".$lname;
         $username_for_student=$_POST["Email"];
         $password_of_student=$_POST["password"];
 
 
 
-    $sql="INSERT INTO user_info(`student_Id`,`branch`,`F_name`,`L_Name`,`user_name`,`password`)
-    values('{$student_ID}','{$branch}','{$fname}','{$lname}','{$username_for_student}','{$password_of_student}')";
+    $sql="INSERT INTO user_info(`student_Id`,`branch`,`name_student`,`user_name`,`password`)
+    values('{$student_ID}','{$branch}','{$full_name}','{$username_for_student}','{$password_of_student}')";
 
-    $query= mysqli_query($connection,$sql) or die("FAIL TO UPLOAD");
+    $query= mysqli_query($connection,$sql) or die("use different username ");
+    
+    header("Location: Login.php");
 
     }
 
